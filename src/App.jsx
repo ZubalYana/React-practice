abd3b03097bc8511b391fc5ef940a7f06a85673e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-08 14:56:54 +0300

    create the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Logo from './components/Logo/Logo'
 
 function App() {
 
   return (
     <>
-    <Header />
+    <Header Logo />
     </>
   )
 }
