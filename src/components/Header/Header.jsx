3a5b81e49f330d2179423b52fa6e92cb3fbc7240
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-08 15:08:57 +0300

    create navigaiton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,14 @@ export default function Header({children}){
     return(
         <div className='Header'>
             {children}
+            <ul>
+                <li>Home</li>
+                <li>Pages</li>
+                <li>Services</li>
+                <li>Projects</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+            </ul>
         </div>
     )
 }
