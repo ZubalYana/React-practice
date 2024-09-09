518d5cabf265923c134a774aed366b7b1a69f076
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-09 22:49:18 +0300

    create the basic main page content

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,16 @@ function App() {
       <Header>
         <Logo />
       </Header>
+      <div className="mainPage">
       <img className='backgroundImg' src="/background.png" alt="" />
+      <div className="mainPage_content">
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="subtitle">INTERIOR</div>
+        <div className="title">Home Designs<br />Made Easy</div>
+        <div className="price">$416.00 <span className='price_starting'>Starting</span></div>
+      </div>
+      </div>
 
     </>
   )
