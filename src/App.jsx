63c2104a42b5e4d4b031b2118dece7d3280e8783
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-09 23:00:33 +0300

    style the main page content

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ function App() {
       <div className="mainPage_content">
         <div className="line1"></div>
         <div className="line2"></div>
+        <div className="text">
         <div className="subtitle">INTERIOR</div>
         <div className="title">Home Designs<br />Made Easy</div>
         <div className="price">$416.00 <span className='price_starting'>Starting</span></div>
+        </div>
       </div>
       </div>
 
