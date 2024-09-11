fdd385966002c32107cf459cec2a1d9701702d48
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-11 22:09:40 +0300

    style and add positions for all the button lines

--- src/components/ShopButton/ShopButton.jsx
+++ src/components/ShopButton/ShopButton.jsx
@@ -4,6 +4,10 @@ export default function Header(){
     return(
         <div className="shopButtonCon">
                 <div className="shopButton">SHOP NOW</div>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+                <div className="line4"></div>
         </div>
     )
 }
