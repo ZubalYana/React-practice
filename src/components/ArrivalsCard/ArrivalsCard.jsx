9432b6a2bf3a93953c550f349ddf8fe9bbc5ad27
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-14 06:54:03 +0300

    create prices and style them

--- src/components/ArrivalsCard/ArrivalsCard.jsx
+++ src/components/ArrivalsCard/ArrivalsCard.jsx
@@ -5,6 +5,10 @@ export default function ArrivalsCard({children}){
         <div className="arrivalsCard">
             <div className="arrivalsCard_imgCon"></div>
             <div className="arrivalsCard_iconsCon">{children}</div>
+            <div className="goodsName">Black chair</div>
+            <div className="priceAndRating">
+                <div className="price"><s>$20.00</s> $15.00</div></div>
+                
         </div>
     )
 }
